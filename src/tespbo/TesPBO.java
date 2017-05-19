c038d339b201d689b1cadfa6faf34b35835d205f
Adinda <Adinda@172.16.12.106>
2017-05-19 20:55:34 +0700

    bismillah

--- src/tespbo/TesPBO.java
+++ src/tespbo/TesPBO.java
@@ -9,8 +9,9 @@ public class TesPBO {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         
+        HashMap<String, ArrayList<data>> peopleByForename = new HashMap<String, ArrayList<data>>();
+        ArrayList<data> people = new ArrayList<data>();
         System.out.println("Daftar Akun Baru");
         System.out.print("Nama Lengkap\t: "); 
         String namaLengkap = new Scanner(System.in).nextLine();
@@ -22,7 +23,38 @@ public class TesPBO {
         String tanggalLahir = new Scanner(System.in).nextLine();
         System.out.print("Hobby\t\t: "); 
         String hobby = new Scanner(System.in).nextLine();
+        people.add(new data("Bagus Adi","Laki-Laki","Sosiologi","2 Juli 1997","Main Game"));
+        people.add(new data("Alfan Aulia Mujib","Laki-Laki","Teknik Informatika","15 Januari 1997","Main Gitar"));
+        people.add(new data("Bagas Aji ","Laki-Laki","Teknik Informatika","17 Agustus 1998","Main Game"));
+        people.add(new data("Kurnia Meiga","Laki-Laki","Teknik Mesin","25 April 1997","Main Bola"));
+        people.add(new data("Adinda Jelfani","Perempuan","Teknik Informatika","17 Oktober 1998","Shopping"));
+        people.add(new data("Nina Isti Andriani","Perempuan","Teknik Informatika","1 Maret 1997","Membaca"));
+        people.add(new data(namaLengkap, jenisKelamin, jurusan, tanggalLahir, hobby));
 
+        ArrayList<data> pilihPeople = new ArrayList<data>();
+        char pilih='y';
+        int i=1;
+        int pilihIndeks=1;
+        while (pilih == 'y'){
+            System.out.println("===========Daftar Semua User============");
+            for (data b:people
+                    ) {
+                System.out.println(i+" "+b.getNamaLengkap());
+                i++;
+            }
+               
+        
+            i=1;
+            System.out.println("");
+            System.out.print("Masukkan Indeks User : ");
+            pilihIndeks = scan.nextInt();
+            pilihPeople.add(people.get(pilihIndeks-=1));
+            scan.nextLine();
+
+            System.out.print("Masukan indeks user lagi? (y/n) : ");
+            pilih = scan.nextLine().charAt(0);
+
+        }
     }
     
 }
