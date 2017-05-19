3793ea3f1c24595c4b18048093563360edca8e5e
Adinda <Adinda@172.16.12.106>
2017-05-19 20:36:37 +0700

    bismillah

--- src/tespbo/TesPBO.java
+++ src/tespbo/TesPBO.java
@@ -1,69 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tespbo;
 
-/**
- *
- * @author Adinda
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class TesPBO {
-public class data {
-    private String namaLengkap;
-    private String jenisKelamin;
-    private String tanggalLahir;
-    private String hobby;
-    private String jurusan;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+        System.out.println("Daftar Akun Baru");
+        System.out.print("Nama Lengkap\t: "); 
+        String namaLengkap = new Scanner(System.in).nextLine();
+        System.out.print("Jenis Kelamin\t: ");
+        String jenisKelamin = new Scanner(System.in).nextLine();
+        System.out.print("Jurusan\t\t: "); 
+        String jurusan = new Scanner(System.in).nextLine();
+        System.out.print("Tanggal Lahir(ex:23 Juli 1997): "); 
+        String tanggalLahir = new Scanner(System.in).nextLine();
+        System.out.print("Hobby\t\t: "); 
+        String hobby = new Scanner(System.in).nextLine();
+
+    }
     
-    public data(String namaLengkap, String jenisKelamin, String jurusan, String tanggalLahir, String hobby) {
-        this.namaLengkap = namaLengkap;
-        this.tanggalLahir = tanggalLahir;
-        this.hobby = hobby;
-        this.jurusan = jurusan;
-        this.jenisKelamin = jenisKelamin;
-    }
-
-    public String getTanggalLahir() {
-        return tanggalLahir;
-    }
-
-    public void setTanggalLahir(String tanggalLahir) {
-        this.tanggalLahir = tanggalLahir;
-    }
-
-    public String getNamaLengkap() {
-        return namaLengkap;
-    }
-
-    public void setNamaLengkap(String namaLengkap) {
-        this.namaLengkap = namaLengkap;
-    }
-
-    public String getHobby() {
-        return hobby;
-    }
-
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
-    }
-
-    public String getJurusan() {
-        return jurusan;
-    }
-
-    public void setJurusan(String jurusan) {
-        this.jurusan = jurusan;
-    }
-
-    public String getJenisKelamin() {
-        return jenisKelamin;
-    }
-
-    public void setJenisKelamin(String jenisKelamin) {
-        this.jenisKelamin = jenisKelamin;
-    }
 }
-}
-
